519e81d4c1714432c7717137b73a8c03f2459533
simon44556 <simon44556@gmail.com>
2022-09-13 21:19:56 +0200

    - Match adaptee properties

--- src/main/java/me/zeromaniac/Adapters/OfflineICPlayerAdapter.java
+++ src/main/java/me/zeromaniac/Adapters/OfflineICPlayerAdapter.java
@@ -4,12 +4,18 @@ import org.bukkit.inventory.EntityEquipment;
 
 import com.loohp.interactivechat.objectholders.OfflineICPlayer;
 
+import alexh.Fluent.Map;
+
 public class OfflineICPlayerAdapter extends OfflineICPlayer {
 
     private OfflineICPlayer player;
+    private Map<String, Object> properties;
 
     public OfflineICPlayerAdapter(OfflineICPlayer player) {
         super(player.getUniqueId(), player.getName(), player.getSelectedSlot(), player.isRightHanded(), player.getExperienceLevel(), player.getInventory(), player.getEnderChest());
+
+        this.properties = (Map<String, Object>) player.getProperties();
+
         this.player = player;
     }
 
